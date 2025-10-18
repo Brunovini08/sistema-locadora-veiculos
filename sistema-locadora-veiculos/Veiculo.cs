1a0939a6add2ed96678bdec952ee762378c0278a
Brunovini08 <brunovinicius082005@gmail.com>
2025-10-18 01:51:23 -0300

    Add project files.

---
+++ sistema-locadora-veiculos/Veiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistema_locadora_veiculos
+{
+    public abstract class Veiculo
+    {
+        private string Marca { get; set; }
+        private string Modelo { get; set; }
+        private int Ano { get; set; }
+        private string Placa { get; set; }
+        private Veiculo(string marca, string modelo, int ano, string placa)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Ano = ano;
+            Placa = placa;
+        }
+        public override string ToString()
+        {
+            return $"Marca: {Marca}, Modelo: {Modelo}, Ano: {Ano}, Placa: {Placa}";
+        }
+    }
+}
