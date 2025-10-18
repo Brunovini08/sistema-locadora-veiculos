ee099665699f34ad6800156834e0eb8c89e3e798
Brunovini08 <brunovinicius082005@gmail.com>
2025-10-18 02:20:28 -0300

    feat: Adicionando a classe moto que recebe Veiculo como herança

---
+++ sistema-locadora-veiculos/Veiculo/Moto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistema_locadora_veiculos.Veiculo
+{
+    public class Moto : Veiculo
+    {
+        private List<string> Cilindrada = new List<string> { "125cc", "150cc", "160cc", "250cc", "300cc", "400cc", "500cc", "600cc", "700cc", "800cc", "900cc", "1000cc"};
+        public Moto(string marca, string modelo, int ano, string placa, List<string> cilindrada) : base(marca, modelo, ano, placa)
+        {
+            Cilindrada = cilindrada;
+        }
+    }
+}
